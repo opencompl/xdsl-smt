27d6be9301f8fdf10e034d376b05e0efc3b7f18c
spica <Spica>
2023-05-16 23:54:38 -0700

    updated dialect usages

--- dialects/index_dialect.py
+++ dialects/index_dialect.py
@@ -1,35 +1,55 @@
+from __future__ import annotations
 from xdsl.ir import Attribute, Dialect, OpResult
 from xdsl.irdl import OpAttr, Operand, irdl_op_definition, IRDLOperation
 
 from traits.effects import Pure
+from abc import ABC
+from z3 import *
 
+from dataclasses import dataclass, field
+from enum import Enum
+from xdsl.dialects.builtin import AnyIntegerAttr
+from xdsl.dialects.builtin import ArrayAttr
+from xdsl.dialects.builtin import IntegerAttr
+from xdsl.dialects.builtin import IndexType
+from xdsl.dialects.builtin import IntegerType
+from xdsl.dialects.builtin import ContainerOf
+from xdsl.dialects.builtin import TypeAttribute
+from xdsl.dialects.builtin import i1
+from xdsl.dialects.arith import Constant
+import functools
+from dataclasses import dataclass
+from enum import Enum
+from typing import Annotated, List, Sequence, TypeVar, Union, Set, Optional
 
 @irdl_op_definition
 class Add(IRDLOperation, Pure):
     name = "index.add"
-    lhs: Operand
-    rhs: Operand
-    result: OpResult
+    lhs: Annotated[Operand, IndexType]
+    rhs: Annotated[Operand, IndexType]
+    result: Annotated[OpResult, IndexType]
 
 @irdl_op_definition
 class And(IRDLOperation, Pure):
     name = "index.and"
-    lhs: Operand
+    lhs: Annotated[Operand, IndexType]
+    rhs: Annotated[Operand, IndexType]
+    result: Annotated[OpResult, IndexType]
 
 @irdl_op_definition
 class Cmp(IRDLOperation, Pure):
     name = "index.cmp"
-    lhs: Operand
-    rhs: Operand
+    lhs: Annotated[Operand, IndexType]
+    rhs: Annotated[Operand, IndexType]
     predicate: OpAttr[Attribute]
-    result: OpResult
+    result: Annotated[OpResult, IndexType]
 
 
 @irdl_op_definition
 class Constant(IRDLOperation, Pure):
     name = "index.constant"
     value: OpAttr[Attribute]
-    result: OpResult
+    result: Annotated[OpResult, IndexType]
 
 Index = Dialect(
     [
--- dialects/transfer.py
+++ dialects/transfer.py
@@ -1,22 +1,8 @@
 from __future__ import annotations
-from abc import ABC
-from z3 import *
 
-from dataclasses import dataclass, field
-from enum import Enum
-from xdsl.dialects.builtin import AnyIntegerAttr
-from xdsl.dialects.builtin import ArrayAttr
-from xdsl.dialects.builtin import IntegerAttr
-from xdsl.dialects.builtin import IndexType
-from xdsl.dialects.builtin import IntegerType
-from xdsl.dialects.builtin import ContainerOf
-from xdsl.dialects.builtin import TypeAttribute
-from xdsl.dialects.builtin import i1
+from xdsl.dialects.builtin import ArrayAttr, IndexType, IntegerType, ContainerOf, TypeAttribute, i1
 from xdsl.dialects.arith import Constant
-import functools
-from dataclasses import dataclass
-from enum import Enum
-from typing import Annotated, List, Sequence, TypeVar, Union, Set, Optional
+from typing import Annotated
 
 from xdsl.ir import (
     ParametrizedAttribute,
@@ -24,15 +10,8 @@ from xdsl.ir import (
 )
 
 from xdsl.irdl import (
-    AllOf,
-    OpAttr,
-    VarOpResult,
     VarOperand,
-    VarRegion,
     irdl_attr_definition,
-    attr_constr_coercion,
-    irdl_data_definition,
-    irdl_to_attr_constraint,
     irdl_op_definition,
     ParameterDef,
     Operand,
@@ -45,7 +24,7 @@ from xdsl.utils.exceptions import VerifyException
 
 @irdl_attr_definition
 class AbstractValueType(ParametrizedAttribute, TypeAttribute):
-    name: str = "absValue"
+    name: str = "abs_value"
 
     fields: ParameterDef[ArrayAttr[IndexType]]
 
@@ -60,47 +39,25 @@ class AbstractValueType(ParametrizedAttribute, TypeAttribute):
             shape = ArrayAttr(shape)
         super().__init__([shape])
 
-
-'''
-    @staticmethod
-    def from_shape(shape: Sequence[int | IntegerAttr[IndexType]]):
-        return AbstractValueType(
-            ArrayAttr(
-                    [
-                        IntegerType.from_width(d)
-                        if isinstance(d, int)
-                        else d
-                        for d in shape
-                    ]))
-'''
-
 signlessIntegerLike = ContainerOf(AnyOf([IntegerType, IndexType]))
 
 
 @irdl_op_definition
 class IfOp(IRDLOperation):
-    def verify_(self) -> None:
-        if len(self.operands) != 3 or len(self.results) != 1:
-            raise VerifyException("IF operation expects 3 operands and 1 result.")
-        if not (self.operands[0].typ == i1):
-            raise VerifyException("IF operand 0 has to be i1 type")
-        if not (self.operands[1].typ == self.operands[2].typ == self.results[0].typ):
-            raise VerifyException("expect all input and result types to be equal")
-
     name: str = "transfer.if"
 
-    cond: Annotated[Operand, ContainerOf(i1)]
+    cond: Annotated[Operand, i1]
     lhs: Annotated[Operand, IndexType]
     rhs: Annotated[Operand, IndexType]
     result: Annotated[OpResult, IndexType]
 
+    def verify_(self) -> None:
+        if not (self.operands[1].typ == self.operands[2].typ == self.results[0].typ):
+            raise VerifyException("expect all input and result types to be equal")
+
 
 @irdl_op_definition
 class NegOp(IRDLOperation):
-    def verify_(self) -> None:
-        if len(self.operands) != 1 or len(self.results) != 1:
-            raise VerifyException("NEG operation expects 1 operands and 1 result.")
-
     name: str = "transfer.neg"
     val: Annotated[Operand, IndexType]
     result: Annotated[Operand, IndexType]
@@ -110,24 +67,14 @@ class NegOp(IRDLOperation):
 class GetOp(IRDLOperation):
     name: str = "transfer.get"
 
-    absVal: Annotated[Operand, ContainerOf(AbstractValueType)]
+    absVal: Annotated[Operand, AbstractValueType]
     index: Annotated[Operand, signlessIntegerLike]
     result: Annotated[OpResult, ContainerOf(IndexType)]
 
     def verify_(self) -> None:
-        if len(self.operands) != 2 or len(self.results) != 1:
-            raise VerifyException("GET operation expects 2 operands and 1 result.")
-        if not isinstance(self.operands[1].op, Constant):
-            raise VerifyException("The second operand has to be arith.constant!")
         index = self.operands[1].op.value.value.data
-        assert isinstance(self.absVal.typ, AbstractValueType)
         if index >= self.absVal.typ.get_num_fields():
             raise VerifyException("The required field is out of range")
-        '''
-        fields=self.absVal.typ.get_fields()
-        if self.results[0].typ!=fields[index]:
-            raise VerifyException("The type of returned value doesn't match accessed value")
-        '''
 
 
 @irdl_op_definition
@@ -141,13 +88,6 @@ class MakeOp(IRDLOperation):
         assert isinstance(self.results[0].typ, AbstractValueType)
         if len(self.operands) != self.results[0].typ.get_num_fields():
             raise VerifyException("The number of given arguments doesn't match the abstract value")
-        '''
-        fields=self.results[0].typ.get_fields()
-        for i in range(len(fields)):
-            if self.operands[i].typ != fields[i]:
-                raise VerifyException("The "+str(i+1)+"th argument doesn't match the abstract value")
-        '''
-
 
 Transfer = Dialect(
     [IfOp, GetOp, MakeOp, NegOp],
