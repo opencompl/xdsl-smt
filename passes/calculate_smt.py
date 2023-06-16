eb4b3c37b7639852ad409a9a37dc6c1f6630dffa
spica <Spica>
2023-06-16 15:53:40 -0700

    verifier added

---
+++ passes/calculate_smt.py
@@ -0,0 +1,223 @@
+from dataclasses import dataclass
+from functools import singledispatch
+
+from xdsl.passes import ModulePass
+
+from xdsl.ir import MLContext, OpResult, BlockArgument
+
+from xdsl.pattern_rewriter import (
+    RewritePattern,
+    PatternRewriter,
+    op_type_rewrite_pattern,
+    PatternRewriteWalker,
+    GreedyRewritePatternApplier,
+)
+from xdsl.dialects import builtin
+
+from utils.integer_to_z3 import *
+from utils.trans_interpreter_smt import *
+
+from z3 import ULT, UGE, ULE, UGT, And, Or, Xor
+
+opToSMTFunc = {
+    "transfer.add": (lambda solver, a, b: BitVecRef.__add__(a, b)),
+    "transfer.sub": (lambda solver, a, b: BitVecRef.__sub__(a, b)),
+    "transfer.neg": (lambda solver, a: BitVecRef.__invert__(a)),
+    "transfer.and": (lambda solver, a, b: BitVecRef.__and__(a, b)),
+    "transfer.or": (lambda solver, a, b: BitVecRef.__or__(a, b)),
+    "transfer.xor": (lambda solver, a, b: BitVecRef.__xor__(a, b)),
+    "transfer.cmp": [
+        (lambda solver, a, b: BitVecRef.__eq__(a, b)),
+        (lambda solver, a, b: BitVecRef.__ne__(a, b)),
+        (lambda solver, a, b: BitVecRef.__lt__(a, b)),
+        (lambda solver, a, b: BitVecRef.__le__(a, b)),
+        (lambda solver, a, b: BitVecRef.__gt__(a, b)),
+        (lambda solver, a, b: BitVecRef.__ge__(a, b)),
+        (lambda solver, a, b: ULT(a, b)),
+        (lambda solver, a, b: ULE(a, b)),
+        (lambda solver, a, b: UGT(a, b)),
+        (lambda solver, a, b: UGE(a, b)),
+    ],
+    "transfer.make": (lambda solver, *operands: operands),
+    "transfer.get": (lambda solver, operands, index: operands[index]),
+    "transfer.constant": (
+        lambda solver, bit_vec, val: get_constant_with_bit_vector(val, bit_vec)
+    ),
+    "arith.andi": (lambda solver, a, b: And(a, b)),
+    "arith.ori": (lambda solver, a, b: Or(a, b)),
+    "arith.xori": (lambda solver, a, b: Xor(a, b)),
+}
+
+funcCall = {}
+
+resultToSMTValue = {}
+
+
+def get_whole_name(op: Operation):
+    parent_op = op.parent_op()
+    if parent_op is not None:
+        assert isinstance(parent_op, FuncOp)
+        return parent_op.sym_name.data + "." + op.results[0].name_hint
+    return None
+
+
+def get_smt_val(op):
+    whole_name = None
+    if isinstance(op, OpResult):
+        op = op.op
+        whole_name = get_whole_name(op)
+    elif isinstance(op, BlockArgument):
+        parent_func = op.block.parent_op()
+        whole_name = parent_func.sym_name.data + "." + op.name_hint
+    if whole_name not in resultToSMTValue:
+        print(whole_name)
+        print(op)
+    assert whole_name in resultToSMTValue
+    return resultToSMTValue[whole_name]
+
+
+@singledispatch
+def to_smt(op, solver, func_name_to_func):
+    if op.name not in opToSMTFunc:
+        print(op)
+        assert False and "not supported operation"
+    if len(op.results) > 0:
+        whole_name = get_whole_name(op)
+        if whole_name not in resultToSMTValue:
+            func = opToSMTFunc[op.name]
+            operands = [get_smt_val(operand) for operand in op.operands]
+            operands = [solver] + operands
+            res = func(*operands)
+            resultToSMTValue[whole_name] = res
+        return resultToSMTValue[whole_name]
+
+
+@to_smt.register
+def _(op: transfer.CmpOp, solver, func_name_to_func):
+    whole_name = get_whole_name(op)
+    if whole_name not in resultToSMTValue:
+        predicate = op.attributes["predicate"].value.data
+        func = opToSMTFunc[op.name][predicate]
+        operands = [get_smt_val(operand) for operand in op.operands]
+        operands = [solver] + operands
+        res = func(*operands)
+        resultToSMTValue[whole_name] = res
+    return resultToSMTValue[whole_name]
+
+
+@to_smt.register
+def _(op: transfer.GetOp, solver, func_name_to_func):
+    whole_name = get_whole_name(op)
+    if whole_name not in resultToSMTValue:
+        operand = get_smt_val(op.operands[0])
+        index = op.attributes["index"].value.data
+        func = opToSMTFunc[op.name]
+        resultToSMTValue[whole_name] = func(solver, operand, index)
+    return resultToSMTValue[whole_name]
+
+
+@to_smt.register
+def _(op: transfer.MakeOp, solver, func_name_to_func):
+    whole_name = get_whole_name(op)
+    if whole_name not in resultToSMTValue:
+        operands = [get_smt_val(operand) for operand in op.operands]
+        func = opToSMTFunc[op.name]
+        resultToSMTValue[whole_name] = func(solver, *operands)
+    return resultToSMTValue[whole_name]
+
+
+@to_smt.register
+def _(op: transfer.Constant, solver, func_name_to_func):
+    whole_name = get_whole_name(op)
+    if whole_name not in resultToSMTValue:
+        bit_vec = get_smt_val(op.operands[0])
+        value = op.attributes["value"].value.data
+        func = opToSMTFunc[op.name]
+        resultToSMTValue[whole_name] = func(solver, bit_vec, value)
+    return resultToSMTValue[whole_name]
+
+
+@to_smt.register
+def _(op: Call, solver, func_name_to_func):
+    callee = op.callee.string_value()
+    if callee not in funcCall:
+        parse_function_to_python(
+            func_name_to_func[callee], func_name_to_func, opToSMTFunc, funcCall
+        )
+    whole_name = get_whole_name(op)
+    if whole_name not in resultToSMTValue:
+        operands = [get_smt_val(operand) for operand in op.operands]
+        func = funcCall[callee]
+        resultToSMTValue[whole_name] = func(solver, *operands)
+    return resultToSMTValue[whole_name]
+
+
+# Special handle on return case
+# Save the returned value with function name
+@to_smt.register
+def _(op: Return, solver, func_name_to_func):
+    whole_name = op.parent_op().sym_name.data + ".return"
+    if whole_name not in resultToSMTValue:
+        smt_val = get_smt_val(op.operands[0])
+        resultToSMTValue[whole_name] = smt_val
+    return resultToSMTValue[whole_name]
+
+
+def init():
+    global resultToSMTValue
+    resultToSMTValue = {}
+
+
+@dataclass
+class AssignAttributes(RewritePattern):
+    def __init__(self, func_name_to_func, solver, width):
+        self.solver = solver
+        self.funcNameToFunc = func_name_to_func
+        self.width = width
+
+    @op_type_rewrite_pattern
+    def match_and_rewrite(self, op: Operation, rewriter: PatternRewriter):
+        if isinstance(op, FuncOp):
+            for arg in op.args:
+                whole_name = op.sym_name.data + "." + arg.name_hint
+                if whole_name not in resultToSMTValue:
+                    tmp_args = None
+                    if isinstance(arg.typ, transfer.AbstractValueType):
+                        tmp_args = []
+                        for j in range(arg.typ.get_num_fields()):
+                            tmp_args.append(
+                                BitVec(arg.name_hint + "field" + str(j), self.width)
+                            )
+                    elif isinstance(arg.typ, builtin.IntegerType):
+                        tmp_args = BitVec(arg.name_hint, arg.typ.width.data)
+                    elif isinstance(arg.typ, builtin.IndexType):
+                        tmp_args = BitVec(arg.name_hint, self.width)
+                    elif isinstance(arg.typ, transfer.TransIntegerType):
+                        tmp_args = BitVec(arg.name_hint, self.width)
+                    else:
+                        print(arg.typ)
+                        assert False and "not supported type"
+                    resultToSMTValue[whole_name] = tmp_args
+        else:
+            to_smt(op, self.solver, self.funcNameToFunc)
+
+
+@dataclass
+class ToSMTAnalysisPass(ModulePass):
+    name = "toSMT"
+
+    def __init__(self, func_name_to_func, solver, width):
+        self.solver = solver
+        self.funcNameToFunc = func_name_to_func
+        self.width = width
+
+    def apply(self, ctx: MLContext, op) -> None:
+        walker = PatternRewriteWalker(
+            GreedyRewritePatternApplier(
+                [AssignAttributes(self.funcNameToFunc, self.solver, self.width)]
+            ),
+            walk_regions_first=False,
+            apply_recursively=True,
+            walk_reverse=False,
+        )
+        walker.rewrite_module(op)
---
+++ verifier.py
@@ -0,0 +1,272 @@
+#!/usr/bin/env python3
+
+import argparse
+
+from xdsl.ir import MLContext, Operation
+from xdsl.parser import Parser
+
+from dialects.smt_dialect import SMTDialect
+from dialects.smt_bitvector_dialect import SMTBitVectorDialect
+from dialects.arith_dialect import Arith
+from dialects.index_dialect import Index
+from dialects.smt_utils_dialect import SMTUtilsDialect
+from xdsl.dialects.builtin import Builtin, ModuleOp
+from xdsl.dialects.func import Func, FuncOp
+from dialects.transfer import Transfer, AbstractValueType
+
+import passes.calculate_smt as cs
+from utils.trans_interpreter_smt import *
+from passes.rename_values import RenameValuesPass
+
+from z3 import BitVec, Solver, And, Not, simplify, ForAll, Implies
+import sys as sys
+
+
+def register_all_arguments(arg_parser: argparse.ArgumentParser):
+    arg_parser.add_argument(
+        "transfer_functions", type=str, nargs="?", help="path to the transfer functions"
+    )
+
+
+def parse_file(file: str | None) -> Operation:
+    if file is None:
+        f = sys.stdin
+    else:
+        f = open(file)
+
+    parser = Parser(ctx, f.read(), file)
+    module = parser.parse_op()
+    return module
+
+
+class Oracle:
+    @staticmethod
+    def get_abs_constraint(func, get_constraint, solver):
+        arg_constraint = []
+        for i, arg in enumerate(func.body.blocks[0].args):
+            if isinstance(arg.typ, AbstractValueType):
+                smt_val = cs.get_smt_val(arg)
+                constraint = get_constraint(solver, smt_val)
+                assert constraint is not None
+                arg_constraint.append(constraint)
+        return arg_constraint
+
+    @staticmethod
+    def get_inst_operands(func: FuncOp, get_instance_constraint, solver, width):
+        inst_list = []
+        inst_constraints = []
+        for i, arg in enumerate(func.body.blocks[0].args):
+            if isinstance(arg.typ, AbstractValueType):
+                inst = BitVec(arg.name_hint + "inst", width)
+                smt_val = cs.get_smt_val(arg)
+                constraint = get_instance_constraint(solver, smt_val, inst)
+                assert constraint is not None
+                inst_constraints.append(constraint)
+                inst_list.append(inst)
+            else:
+                inst_list.append(getattr(arg, "smtVal"))
+        return inst_list, inst_constraints
+
+
+def basic_constraint_check(absOp, ctx, get_constraint, func_name_to_func, width):
+    s = Solver()
+    cs.init()
+    cs.ToSMTAnalysisPass(func_name_to_func, s, width).apply(ctx, absOp)
+    abs_constraint = Oracle.get_abs_constraint(absOp, get_constraint, s)
+    s.add(And(abs_constraint))
+    key = absOp.sym_name.data + ".return"
+    assert key in cs.resultToSMTValue
+    smt_val = cs.resultToSMTValue[key]
+    constraint = get_constraint(s, smt_val)
+    s.add(Not(And(constraint)))
+    check_res = s.check()
+    if str(check_res) == "sat":
+        print("basic constraint check failed!\ncounterexample:\n")
+        print(s.model())
+        return -1
+    elif str(check_res) == "unsat":
+        print("basic constraint check successfully")
+        return 1
+    else:
+        print("unknown: ", check_res)
+        return 0
+
+
+def soundness_check(
+    concrete_op,
+    abs_op,
+    ctx,
+    get_constraint,
+    get_instance_constraint,
+    func_name_to_func,
+    width,
+):
+    s = Solver()
+    cs.init()
+    cs.ToSMTAnalysisPass(func_name_to_func, s, width).apply(ctx, abs_op)
+    concrete_func = parse_function_to_python(
+        concrete_op, func_name_to_func, cs.opToSMTFunc, cs.funcCall
+    )
+    abs_constraint = Oracle.get_abs_constraint(abs_op, get_constraint, s)
+    s.add(And(abs_constraint))
+    key = abs_op.sym_name.data + ".return"
+    assert key in cs.resultToSMTValue
+    abs_op_result = cs.resultToSMTValue[key]
+    assert abs_op_result is not None
+    inst_list, inst_constraints = Oracle.get_inst_operands(
+        abs_op, get_instance_constraint, s, width
+    )
+    s.add(inst_constraints)
+
+    inst_result_smt = concrete_func(s, *inst_list)
+
+    constraint = get_instance_constraint(s, abs_op_result, inst_result_smt)
+
+    s.add(simplify(Not(And(constraint))))
+    check_res = s.check()
+    if str(check_res) == "sat":
+        print("soundness check failed!\ncounterexample:\n")
+        print(s.model())
+        return -1
+    elif str(check_res) == "unsat":
+        print("soundness check successfully")
+        return 1
+    else:
+        print("unknown: ", check_res)
+        return 0
+
+
+def precision_check(
+    concrete_op: FuncOp,
+    abs_op,
+    ctx,
+    get_constraint,
+    get_instance_constraint,
+    func_name_to_func,
+    width,
+):
+    s = Solver()
+    cs.ToSMTAnalysisPass(func_name_to_func, s, width).apply(ctx, abs_op)
+
+    concrete_func = parse_function_to_python(
+        concrete_op, func_name_to_func, cs.opToSMTFunc, cs.funcCall
+    )
+    abs_constraint = Oracle.get_abs_constraint(abs_op, get_constraint, s)
+    s.add(And(abs_constraint))
+    key = abs_op.sym_name.data + ".return"
+    assert key in cs.resultToSMTValue
+    abs_result = cs.resultToSMTValue[key]
+    assert abs_result is not None
+    abs_result_constraint = get_constraint(s, abs_result)
+    s.add(abs_result_constraint)
+
+    abs_result_inst = BitVec("absResultInst", width)
+    abs_result_inst_constraint = get_instance_constraint(s, abs_result, abs_result_inst)
+    s.add(abs_result_inst_constraint)
+
+    inst_list, inst_constraints = Oracle.get_inst_operands(
+        abs_op, get_instance_constraint, s, width
+    )
+    concrete_result = concrete_func(s, *inst_list)
+    # for now
+    qualifier = inst_list
+    s.add(
+        ForAll(
+            qualifier,
+            Implies(And(inst_constraints), abs_result_inst != concrete_result),
+        ),
+    )
+    check_res = s.check()
+    if str(check_res) == "sat":
+        print("precision check failed!\ncounterexample:\n")
+        print(s.model())
+        return -1
+    elif str(check_res) == "unsat":
+        print("precision check successfully")
+        return 1
+    else:
+        print("unknown: ", check_res)
+        return 0
+
+
+KEY_NEED_VERIFY = "builtin.NEED_VERIFY"
+MAXIMAL_VERIFIED_BITS = 8
+
+if __name__ == "__main__":
+    ctx = MLContext()
+    arg_parser = argparse.ArgumentParser()
+    register_all_arguments(arg_parser)
+    args = arg_parser.parse_args()
+
+    # Register all dialects
+    ctx.register_dialect(Arith)
+    ctx.register_dialect(Builtin)
+    ctx.register_dialect(Func)
+    ctx.register_dialect(SMTDialect)
+    ctx.register_dialect(SMTBitVectorDialect)
+    ctx.register_dialect(SMTUtilsDialect)
+    ctx.register_dialect(Transfer)
+    ctx.register_dialect(Index)
+
+    # Parse the files
+    module = parse_file(args.transfer_functions)
+    assert isinstance(module, ModuleOp)
+
+    get_constraint = None
+    get_instance_constraint = None
+
+    func_name_to_func = {}
+
+    RenameValuesPass().apply(ctx, module)
+    for func in module.ops:
+        if isinstance(func, FuncOp):
+            func_name_to_func[func.sym_name.data] = func
+            if func.sym_name.data == "getConstraint":
+                get_constraint = func
+            elif func.sym_name.data == "getInstanceConstraint":
+                get_instance_constraint = func
+
+    get_constraint = parse_function_to_python(
+        get_constraint, func_name_to_func, cs.opToSMTFunc, cs.funcCall
+    )
+    get_instance_constraint = parse_function_to_python(
+        get_instance_constraint, func_name_to_func, cs.opToSMTFunc, cs.funcCall
+    )
+
+    NEED_VERIFY = []
+
+    for pair in module.attributes[KEY_NEED_VERIFY]:
+        assert len(pair) == 2 and "concrete and abstract operation should be paired"
+        NEED_VERIFY.append([])
+        for p in pair:
+            NEED_VERIFY[-1].append(p.data)
+            assert p.data in func_name_to_func and "Cannot find the specified function"
+        for width in range(1, MAXIMAL_VERIFIED_BITS + 1):
+            print(
+                "Currently verifying: ", NEED_VERIFY[-1][0], " with bitwidth, ", width
+            )
+            basic_constraint_check(
+                func_name_to_func[NEED_VERIFY[-1][1]],
+                ctx,
+                get_constraint,
+                func_name_to_func,
+                width,
+            )
+            soundness_check(
+                func_name_to_func[NEED_VERIFY[-1][0]],
+                func_name_to_func[NEED_VERIFY[-1][1]],
+                ctx,
+                get_constraint,
+                get_instance_constraint,
+                func_name_to_func,
+                width,
+            )
+            precision_check(
+                func_name_to_func[NEED_VERIFY[-1][0]],
+                func_name_to_func[NEED_VERIFY[-1][1]],
+                ctx,
+                get_constraint,
+                get_instance_constraint,
+                func_name_to_func,
+                width,
+            )
