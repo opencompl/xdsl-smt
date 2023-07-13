03786267ca5af3b82b1fa1fc09530a727ec626c2
Mathieu Fehr <mathieu.fehr@gmail.com>
2023-07-13 14:32:27 +0100

    Allow the transfer dialect to take concrete types

--- dialects/transfer.py
+++ dialects/transfer.py
@@ -4,6 +4,7 @@ from xdsl.dialects.builtin import (
     ArrayAttr,
     IndexType,
     IntegerAttr,
+    IntegerType,
     i1,
 )
 from typing import Annotated
@@ -11,6 +12,7 @@ from typing import Annotated
 from xdsl.ir import ParametrizedAttribute, Dialect, TypeAttribute, OpResult, Attribute
 
 from xdsl.irdl import (
+    ConstraintVar,
     OpAttr,
     Operand,
     VarOperand,
@@ -30,163 +32,226 @@ class TransIntegerType(ParametrizedAttribute, TypeAttribute):
 @irdl_op_definition
 class Constant(IRDLOperation):
     name = "transfer.constant"
-    op: Annotated[Operand, TransIntegerType]
-    result: Annotated[OpResult, TransIntegerType]
+
+    T = Annotated[TransIntegerType | IntegerType, ConstraintVar("T")]
+
+    op: Annotated[Operand, T]
+    result: Annotated[OpResult, T]
     value: OpAttr[IntegerAttr[IndexType]]
 
 
 @irdl_op_definition
 class NegOp(IRDLOperation):
     name = "transfer.neg"
-    op: Annotated[Operand, TransIntegerType]
-    result: Annotated[OpResult, TransIntegerType]
+
+    T = Annotated[TransIntegerType | IntegerType, ConstraintVar("T")]
+
+    op: Annotated[Operand, T]
+    result: Annotated[OpResult, T]
 
 
 @irdl_op_definition
 class AddOp(IRDLOperation):
     name = "transfer.add"
-    lhs: Annotated[Operand, TransIntegerType]
-    rhs: Annotated[Operand, TransIntegerType]
-    result: Annotated[OpResult, TransIntegerType]
+
+    T = Annotated[TransIntegerType | IntegerType, ConstraintVar("T")]
+
+    lhs: Annotated[Operand, T]
+    rhs: Annotated[Operand, T]
+    result: Annotated[OpResult, T]
 
 
 @irdl_op_definition
 class SubOp(IRDLOperation):
     name = "transfer.sub"
-    lhs: Annotated[Operand, TransIntegerType]
-    rhs: Annotated[Operand, TransIntegerType]
-    result: Annotated[OpResult, TransIntegerType]
+
+    T = Annotated[TransIntegerType | IntegerType, ConstraintVar("T")]
+
+    lhs: Annotated[Operand, T]
+    rhs: Annotated[Operand, T]
+    result: Annotated[OpResult, T]
 
 
 @irdl_op_definition
 class MulOp(IRDLOperation):
     name = "transfer.mul"
-    lhs: Annotated[Operand, TransIntegerType]
-    rhs: Annotated[Operand, TransIntegerType]
-    result: Annotated[OpResult, TransIntegerType]
+
+    T = Annotated[TransIntegerType | IntegerType, ConstraintVar("T")]
+
+    lhs: Annotated[Operand, T]
+    rhs: Annotated[Operand, T]
+    result: Annotated[OpResult, T]
 
 
 @irdl_op_definition
 class UMulOverflowOp(IRDLOperation):
     name = "transfer.umul_overflow"
-    lhs: Annotated[Operand, TransIntegerType]
-    rhs: Annotated[Operand, TransIntegerType]
+
+    T = Annotated[TransIntegerType | IntegerType, ConstraintVar("T")]
+
+    lhs: Annotated[Operand, T]
+    rhs: Annotated[Operand, T]
     result: Annotated[OpResult, i1]
 
 
 @irdl_op_definition
 class AndOp(IRDLOperation):
     name = "transfer.and"
-    lhs: Annotated[Operand, TransIntegerType]
-    rhs: Annotated[Operand, TransIntegerType]
-    result: Annotated[OpResult, TransIntegerType]
+
+    T = Annotated[TransIntegerType | IntegerType, ConstraintVar("T")]
+
+    lhs: Annotated[Operand, T]
+    rhs: Annotated[Operand, T]
+    result: Annotated[OpResult, T]
 
 
 @irdl_op_definition
 class OrOp(IRDLOperation):
     name = "transfer.or"
-    lhs: Annotated[Operand, TransIntegerType]
-    rhs: Annotated[Operand, TransIntegerType]
-    result: Annotated[OpResult, TransIntegerType]
+
+    T = Annotated[TransIntegerType | IntegerType, ConstraintVar("T")]
+
+    lhs: Annotated[Operand, T]
+    rhs: Annotated[Operand, T]
+    result: Annotated[OpResult, T]
 
 
 @irdl_op_definition
 class XorOp(IRDLOperation):
     name = "transfer.xor"
-    lhs: Annotated[Operand, TransIntegerType]
-    rhs: Annotated[Operand, TransIntegerType]
-    result: Annotated[OpResult, TransIntegerType]
+
+    T = Annotated[TransIntegerType | IntegerType, ConstraintVar("T")]
+
+    lhs: Annotated[Operand, T]
+    rhs: Annotated[Operand, T]
+    result: Annotated[OpResult, T]
 
 
 @irdl_op_definition
 class GetBitWidthOp(IRDLOperation):
     name = "transfer.get_bit_width"
-    val: Annotated[Operand, TransIntegerType]
-    result: Annotated[OpResult, TransIntegerType]
+
+    T = Annotated[TransIntegerType | IntegerType, ConstraintVar("T")]
+
+    val: Annotated[Operand, T]
+    result: Annotated[OpResult, T]
 
 
 @irdl_op_definition
 class CountLZeroOp(IRDLOperation):
     name = "transfer.countl_zero"
-    val: Annotated[Operand, TransIntegerType]
-    result: Annotated[OpResult, TransIntegerType]
+
+    T = Annotated[TransIntegerType | IntegerType, ConstraintVar("T")]
+
+    val: Annotated[Operand, T]
+    result: Annotated[OpResult, T]
 
 
 @irdl_op_definition
 class CountRZeroOp(IRDLOperation):
     name = "transfer.countr_zero"
-    val: Annotated[Operand, TransIntegerType]
-    result: Annotated[OpResult, TransIntegerType]
+
+    T = Annotated[TransIntegerType | IntegerType, ConstraintVar("T")]
+
+    val: Annotated[Operand, T]
+    result: Annotated[OpResult, T]
 
 
 @irdl_op_definition
 class CountLOneOp(IRDLOperation):
     name = "transfer.countl_one"
-    val: Annotated[Operand, TransIntegerType]
-    result: Annotated[OpResult, TransIntegerType]
+
+    T = Annotated[TransIntegerType | IntegerType, ConstraintVar("T")]
+
+    val: Annotated[Operand, T]
+    result: Annotated[OpResult, T]
 
 
 @irdl_op_definition
 class CountROneOp(IRDLOperation):
     name = "transfer.countr_one"
-    val: Annotated[Operand, TransIntegerType]
-    result: Annotated[OpResult, TransIntegerType]
+
+    T = Annotated[TransIntegerType | IntegerType, ConstraintVar("T")]
+
+    val: Annotated[Operand, T]
+    result: Annotated[OpResult, T]
 
 
 @irdl_op_definition
 class SMinOp(IRDLOperation):
     name = "transfer.smin"
-    lhs: Annotated[Operand, TransIntegerType]
-    rhs: Annotated[Operand, TransIntegerType]
-    result: Annotated[OpResult, TransIntegerType]
+
+    T = Annotated[TransIntegerType | IntegerType, ConstraintVar("T")]
+
+    lhs: Annotated[Operand, T]
+    rhs: Annotated[Operand, T]
+    result: Annotated[OpResult, T]
 
 
 @irdl_op_definition
 class SMaxOp(IRDLOperation):
     name = "transfer.smax"
-    lhs: Annotated[Operand, TransIntegerType]
-    rhs: Annotated[Operand, TransIntegerType]
-    result: Annotated[OpResult, TransIntegerType]
+
+    T = Annotated[TransIntegerType | IntegerType, ConstraintVar("T")]
+
+    lhs: Annotated[Operand, T]
+    rhs: Annotated[Operand, T]
+    result: Annotated[OpResult, T]
 
 
 @irdl_op_definition
 class UMinOp(IRDLOperation):
     name = "transfer.umin"
-    lhs: Annotated[Operand, TransIntegerType]
-    rhs: Annotated[Operand, TransIntegerType]
-    result: Annotated[OpResult, TransIntegerType]
+
+    T = Annotated[TransIntegerType | IntegerType, ConstraintVar("T")]
+
+    lhs: Annotated[Operand, T]
+    rhs: Annotated[Operand, T]
+    result: Annotated[OpResult, T]
 
 
 @irdl_op_definition
 class UMaxOp(IRDLOperation):
     name = "transfer.umax"
-    lhs: Annotated[Operand, TransIntegerType]
-    rhs: Annotated[Operand, TransIntegerType]
-    result: Annotated[OpResult, TransIntegerType]
+
+    T = Annotated[TransIntegerType | IntegerType, ConstraintVar("T")]
+
+    lhs: Annotated[Operand, T]
+    rhs: Annotated[Operand, T]
+    result: Annotated[OpResult, T]
 
 
 @irdl_op_definition
 class GetLowBitsOp(IRDLOperation):
     name = "transfer.get_low_bits"
-    val: Annotated[Operand, TransIntegerType]
-    low_bits: Annotated[Operand, TransIntegerType]
-    result: Annotated[OpResult, TransIntegerType]
+
+    T = Annotated[TransIntegerType | IntegerType, ConstraintVar("T")]
+
+    val: Annotated[Operand, T]
+    low_bits: Annotated[Operand, T]
+    result: Annotated[OpResult, T]
 
 
 @irdl_op_definition
 class SetHighBitsOp(IRDLOperation):
     name = "transfer.set_high_bits"
-    val: Annotated[Operand, TransIntegerType]
-    high_bits: Annotated[Operand, TransIntegerType]
-    result: Annotated[OpResult, TransIntegerType]
+
+    T = Annotated[TransIntegerType | IntegerType, ConstraintVar("T")]
+
+    val: Annotated[Operand, T]
+    high_bits: Annotated[Operand, T]
+    result: Annotated[OpResult, T]
 
 
 @irdl_op_definition
 class CmpOp(IRDLOperation):
     name = "transfer.cmp"
+
+    T = Annotated[TransIntegerType | IntegerType, ConstraintVar("T")]
+
     predicate: OpAttr[IntegerAttr[IndexType]]
-    lhs: Annotated[Operand, TransIntegerType]
-    rhs: Annotated[Operand, TransIntegerType]
+    lhs: Annotated[Operand, T]
+    rhs: Annotated[Operand, T]
     result: Annotated[OpResult, i1]
 
 
@@ -213,27 +278,31 @@ class GetOp(IRDLOperation):
 
     abs_val: Annotated[Operand, AbstractValueType]
     index: OpAttr[IntegerAttr[IndexType]]
-    result: Annotated[OpResult, IndexType]
+    result: Annotated[OpResult, Attribute]
 
     def verify_(self) -> None:
         assert isinstance(self.abs_val.typ, AbstractValueType)
         if self.index.value.data >= self.abs_val.typ.get_num_fields():
             raise VerifyException("The required field is out of range")
+        if self.abs_val.typ.get_fields()[self.index.value.data] != self.result.typ:
+            raise VerifyException("The required field doesn't match the result type")
 
 
 @irdl_op_definition
 class MakeOp(IRDLOperation):
     name = "transfer.make"
 
-    arguments: Annotated[VarOperand, IndexType]
+    arguments: Annotated[VarOperand, Attribute]
     result: Annotated[OpResult, AbstractValueType]
 
     def verify_(self) -> None:
-        assert isinstance(self.results[0].typ, AbstractValueType)
-        if len(self.operands) != self.results[0].typ.get_num_fields():
+        assert isinstance(self.result.typ, AbstractValueType)
+        if len(self.operands) != self.result.typ.get_num_fields():
             raise VerifyException(
                 "The number of given arguments doesn't match the abstract value"
             )
+        if self.result.typ.get_fields() != [arg.typ for arg in self.arguments]:
+            raise VerifyException("The required field doesn't match the result type")
 
 
 Transfer = Dialect(
