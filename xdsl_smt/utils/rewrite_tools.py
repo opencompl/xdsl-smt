8ccb7547cb7250d16f84a11ec4ac4f6eee472c7b
Chloe Pronovost <chloe.pronovost@pm.me>
2023-09-14 13:37:23 -0600

    Add iterator over new (unparented) ops

---
+++ xdsl_smt/utils/rewrite_tools.py
@@ -0,0 +1,13 @@
+from typing import Iterable
+
+from xdsl.ir import (Operation, OpResult)
+
+
+def new_ops(op: Operation) -> Iterable[Operation]:
+    """Iterate over an un-parented operation and its operands in post-order (operands first)"""
+
+    if op.parent is None:
+        for child_op in op.operands:
+            if isinstance(child_op, OpResult):
+                yield from new_ops(child_op.op)
+        yield op
