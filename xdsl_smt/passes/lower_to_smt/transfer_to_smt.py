2eea9e999439f5cc43c83d8d4c12f7d2204033dc
Yuyou Fan <spicaofsky@gmail.com>
2023-09-27 14:30:58 -0600

    lower functions added

--- xdsl_smt/passes/lower_to_smt/transfer_to_smt.py
+++ xdsl_smt/passes/lower_to_smt/transfer_to_smt.py
@@ -1,11 +1,33 @@
 from xdsl.irdl import IRDLOperation
-from xdsl.pattern_rewriter import RewritePattern, PatternRewriter
+from xdsl.pattern_rewriter import (
+    PatternRewriter,
+    RewritePattern,
+    op_type_rewrite_pattern,
+)
 from xdsl.ir import Operation
 
 from ...dialects import smt_bitvector_dialect as smt_bv
 from ...dialects import smt_dialect as smt
 from ...dialects import transfer
+from xdsl.ir import Attribute, MLContext
+from .lower_to_smt import LowerToSMT
+from ...dialects.smt_utils_dialect import PairType
+from xdsl_smt.dialects.smt_dialect import BoolType
 
+def abstract_value_type_lowerer(type: Attribute) -> PairType | None:
+    """Lower all types in an abstract value to SMT types"""
+    if isinstance(type, transfer.AbstractValueType):
+        result=PairType(LowerToSMT.lower_type(type.get_fields()[-1]), BoolType())
+        for ty in reversed(type.get_fields()[:-1]):
+            result=PairType(LowerToSMT.lower_type(ty),result)
+        return result
+    return None
+
+
+def transfer_integer_type_lowerer(type: Attribute, width: int) -> smt_bv.BitVectorType | None:
+    if isinstance(type, transfer.TransIntegerType):
+        return smt_bv.BitVectorType(width)
+    return None
 
 def trivial_pattern(
     match_type: type[IRDLOperation], rewrite_type: type[IRDLOperation]
@@ -37,19 +59,21 @@ class CmpOpPattern(RewritePattern):
               smt_bv.UgeOp,
     ]
 
+    @op_type_rewrite_pattern
     def match_and_rewrite(self, op: transfer.CmpOp, rewriter: PatternRewriter):
         predicate = op.attributes["predicate"].value.data
         #Neq -> Not(Eq....)
-        if(predicate==1):
+        if 1==predicate:
             tmp_op=smt.EqOp.create(
                 operands=op.operands,
                 result_types=[op.operands[0].type],
             )
+            rewriter.insert_op_before_matched_op(tmp_op)
             op=tmp_op
 
         rewrite_type = self.new_ops[predicate]
         new_op = rewrite_type.create(
-            operands=op.operands,
+            operands=op.results,
             result_types=[op.operands[0].type],
         )
         rewriter.replace_matched_op([new_op])
