6d670cb17e82fba9ab01ad46a169ef68011dd3ee
Mathieu Fehr <mathieu.fehr@gmail.com>
2024-03-26 22:37:02 +0000

    Fix wrong __init__.py

--- xdsl_smt/passes/lower_to_smt/__init__.py
+++ xdsl_smt/passes/lower_to_smt/__init__.py
@@ -1,18 +1,10 @@
 from .lower_to_smt import *
 
-from .arith_semantics import (
-    arith_semantics,  # pyright: ignore[reportUnusedImport]
-)
-from .comb_semantics import comb_semantics  # pyright: ignore[reportUnusedImport]
-
-# from .transfer_to_smt import (
-#    transfer_to_smt_patterns,  # pyright: ignore[reportUnusedImport]
-# )
-from .transfer_semantics import (
-    transfer_semantics,
-)
 from .func_to_smt import func_to_smt_patterns  # pyright: ignore[reportUnusedImport]
 from .llvm_to_smt import llvm_to_smt_patterns  # pyright: ignore[reportUnusedImport]
+from .transfer_to_smt import (
+    transfer_to_smt_patterns,
+)  # pyright: ignore[reportUnusedImport]
 
 LowerToSMT.rewrite_patterns = [
     *transfer_to_smt_patterns,
