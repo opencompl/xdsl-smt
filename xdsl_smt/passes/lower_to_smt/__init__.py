26dfd86fe06ced4a23d869c5ba8391d7263e538b
Fehr Mathieu <mathieu.fehr@gmail.com>
2024-10-12 08:15:58 +0100

    Export properly llvm operations (#30)

--- xdsl_smt/passes/lower_to_smt/__init__.py
+++ xdsl_smt/passes/lower_to_smt/__init__.py
@@ -1,9 +1,13 @@
 from .lower_to_smt import *
 
-from .func_to_smt import func_to_smt_patterns  # pyright: ignore[reportUnusedImport]
-from .llvm_to_smt import llvm_to_smt_patterns  # pyright: ignore[reportUnusedImport]
+from .func_to_smt import (
+    func_to_smt_patterns as func_to_smt_patterns,
+)
+from .llvm_to_smt import (
+    llvm_to_smt_patterns as llvm_to_smt_patterns,
+)
 from .transfer_to_smt import (
-    transfer_to_smt_patterns,
-)  # pyright: ignore[reportUnusedImport]
+    transfer_to_smt_patterns as transfer_to_smt_patterns,
+)
 
 SMTLowerer.rewrite_patterns = {**func_to_smt_patterns, **transfer_to_smt_patterns}
