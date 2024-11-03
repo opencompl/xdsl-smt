b229606d13f95725f4d05a4b7b5292d764b0e248
Mathieu Fehr <mathieu.fehr@gmail.com>
2024-11-03 02:40:08 +0000

    Add lowering of memory effect pointer offset to memory dialect

--- tests/filecheck/lower-effects-with-memory.mlir
+++ tests/filecheck/lower-effects-with-memory.mlir
@@ -1,15 +1,29 @@
-// RUN: xdsl-smt %s -p=lower-effects-with-memory --split-input-file | filecheck %s
+// RUN: xdsl-smt %s -p=lower-effects-with-memory,lower-pairs --split-input-file | filecheck %s
 
 %state = "smt.declare_const"() : () -> !effect.state
-%size = "smt.bv.constant"() {value = #smt.bv.bv_val<32 : 64>} : () -> !smt.bv.bv<64>
+%size = "smt.declare_const"() : () -> !smt.bv.bv<64>
 %state2, %ptr = mem_effect.alloc %state, %size
 
 // CHECK:         %state_first = "smt.declare_const"() : () -> !memory.memory
 // CHECK-NEXT:    %state_second = "smt.declare_const"() : () -> !smt.bool
-// CHECK-NEXT:    %size = "smt.bv.constant"() {"value" = #smt.bv.bv_val<32: 64>} : () -> !smt.bv.bv<64>
+// CHECK-NEXT:    %size = "smt.declare_const"() : () -> !smt.bv.bv<64>
 // CHECK-NEXT:    %bid = memory.get_fresh_block_id %state_first
 // CHECK-NEXT:    %block = memory.get_block %state_first[%bid]
 // CHECK-NEXT:    %0 = "smt.constant_bool"() {"value" = #smt.bool_attr<true>} : () -> !smt.bool
 // CHECK-NEXT:    %block_1 = memory.set_block_live_marker %block, %0
 // CHECK-NEXT:    %block_2 = memory.set_block_size %block_1, %size
 // CHECK-NEXT:    %memory = memory.set_block %state_first[%bid], %block_2
+
+// -----
+
+%ptr = "smt.declare_const"() : () -> !mem_effect.ptr
+%offset = "smt.declare_const"() : () -> !smt.bv.bv<64>
+%ptr2 = mem_effect.offset_ptr %ptr[%offset]
+"test.op"(%ptr2) : (!mem_effect.ptr) -> ()
+
+// CHECK:         %ptr_first = "smt.declare_const"() : () -> !memory.block_id
+// CHECK-NEXT:    %ptr_second = "smt.declare_const"() : () -> !smt.bv.bv<64>
+// CHECK-NEXT:    %offset = "smt.declare_const"() : () -> !smt.bv.bv<64>
+// CHECK-NEXT:    %ptr_offset = "smt.bv.add"(%ptr_second, %offset) : (!smt.bv.bv<64>, !smt.bv.bv<64>) -> !smt.bv.bv<64>
+// CHECK-NEXT:    %ptr = "smt.utils.pair"(%ptr_first, %ptr_offset) : (!memory.block_id, !smt.bv.bv<64>) -> !smt.utils.pair<!memory.block_id, !smt.bv.bv<64>>
+// CHECK-NEXT:    "test.op"(%ptr) : (!smt.utils.pair<!memory.block_id, !smt.bv.bv<64>>) -> ()
--- xdsl_smt/passes/lower_effects_with_memory.py
+++ xdsl_smt/passes/lower_effects_with_memory.py
@@ -14,9 +14,9 @@ from xdsl.pattern_rewriter import (
 
 from xdsl.dialects.builtin import ModuleOp, AnyArrayAttr
 from xdsl_smt.dialects.effects import memory_effect as mem_effect
-from xdsl_smt.dialects.effects.effect import StateType
 from xdsl_smt.dialects.smt_bitvector_dialect import BitVectorType
 from xdsl_smt.dialects.smt_dialect import BoolType
+from xdsl_smt.dialects.effects import effect, memory_effect as mem_effect
 from xdsl_smt.dialects import (
     memory_dialect as mem,
     smt_utils_dialect as smt_utils,
@@ -46,7 +46,30 @@ def create_state(memory: SSAValue, ub: SSAValue, rewriter: PatternRewriter) -> S
     """Create a state value from the ub flag and memory state."""
     state = smt_utils.PairOp(memory, ub)
     rewriter.insert_op_before_matched_op([state])
+    state.res.name_hint = "state"
     return state.res
+
+
+def get_block_id_and_offset_from_pointer(
+    pointer: SSAValue, rewriter: PatternRewriter
+) -> tuple[SSAValue, SSAValue]:
+    """Get the block ID and offset from a pointer value."""
+    block_id = smt_utils.FirstOp(pointer, new_pointer_type)
+    offset = smt_utils.SecondOp(pointer, new_pointer_type)
+    rewriter.insert_op_before_matched_op([block_id, offset])
+    block_id.res.name_hint = "block_id"
+    offset.res.name_hint = "ptr_offset"
+    return block_id.res, offset.res
+
+
+def create_pointer(
+    block_id: SSAValue, offset: SSAValue, rewriter: PatternRewriter
+) -> SSAValue:
+    """Create a pointer value from the block ID and offset."""
+    pointer = smt_utils.PairOp(block_id, offset)
+    rewriter.insert_op_before_matched_op([pointer])
+    pointer.res.name_hint = "ptr"
+    return pointer.res
 
 
 def recursively_convert_attr(attr: Attribute) -> Attribute:
@@ -54,8 +77,10 @@ def recursively_convert_attr(attr: Attribute) -> Attribute:
     Recursively convert an attribute to replace all references to the effect state
     into a pair between the ub flag and the memory.
     """
-    if isinstance(attr, StateType):
+    if isinstance(attr, effect.StateType):
         return smt_utils.PairType(mem.MemoryType(), BoolType())
+    if isinstance(attr, mem_effect.PointerType):
+        return smt_utils.PairType(mem.BlockIDType(), BitVectorType(64))
     if isinstance(attr, ParametrizedAttribute):
         return type(attr).new(
             [recursively_convert_attr(param) for param in attr.parameters]
@@ -122,14 +147,34 @@ class LowerAlloc(RewritePattern):
 
         # Get a pointer to the block
         zero_index_op = smt_bv.ConstantOp(0, 64)
-        new_pointer_op = smt_utils.PairOp(id_op.res, zero_index_op.res)
-        rewriter.insert_op_before_matched_op([zero_index_op, new_pointer_op])
+        rewriter.insert_op_before_matched_op([zero_index_op])
+        new_pointer = create_pointer(id_op.res, zero_index_op.res, rewriter)
 
         # Get the new state
         new_state = create_state(new_memory_op.res, ub, rewriter)
 
         # Replace the matched operation
-        rewriter.replace_matched_op([], [new_state, new_pointer_op.res])
+        rewriter.replace_matched_op([], [new_state, new_pointer])
+
+
+class LowerPointerOffset(RewritePattern):
+    @op_type_rewrite_pattern
+    def match_and_rewrite(
+        self, op: mem_effect.OffsetPointerOp, rewriter: PatternRewriter
+    ):
+        # Get the block ID and offset from the pointer
+        block_id, offset = get_block_id_and_offset_from_pointer(op.pointer, rewriter)
+
+        # Add the offset to the given offset
+        new_offset_op = smt_bv.AddOp(offset, op.offset)
+        new_offset_op.res.name_hint = "ptr_offset"
+        rewriter.insert_op_before_matched_op([new_offset_op])
+
+        # Create the new pointer
+        new_pointer = create_pointer(block_id, new_offset_op.res, rewriter)
+
+        # Replace the matched operation
+        rewriter.replace_matched_op([], [new_pointer])
 
 
 @dataclass(frozen=True)
@@ -139,7 +184,13 @@ class LowerEffectWithMemoryPass(ModulePass):
     def apply(self, ctx: MLContext, op: ModuleOp) -> None:
         walker = PatternRewriteWalker(
             GreedyRewritePatternApplier(
-                [LowerTriggerOp(), LowerToBoolOp(), LowerGenericOp(), LowerAlloc()]
+                [
+                    LowerTriggerOp(),
+                    LowerToBoolOp(),
+                    LowerGenericOp(),
+                    LowerAlloc(),
+                    LowerPointerOffset(),
+                ]
             )
         )
         walker.rewrite_module(op)
