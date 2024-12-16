b609f81a337e8cb00d38562336e9de2a72e94485
Hugo Pompougnac <hugo.pompougnac@inria.fr>
2024-12-16 22:24:57 +0000

    Make pyright happy

--- xdsl_smt/passes/load_parametric_int_semantics.py
+++ xdsl_smt/passes/load_parametric_int_semantics.py
@@ -12,6 +12,12 @@ class LoadIntSemanticsPass(ModulePass):
     name = "load-int-semantics"
 
     def apply(self, ctx: MLContext, op: ModuleOp) -> None:
+        if op.body.first_block is None:
+            return
+        assert op.body.first_block
+        if op.body.first_block.first_op:
+            return
+        assert op.body.first_block.first_op
         rewriter = PatternRewriter(op.body.first_block.first_op)
         integer_proxy = IntegerProxy()
         integer_proxy.build_pow2(rewriter)
