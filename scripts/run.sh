f433fd3f486d03e53ab9cd2544248522f8885b66
spica <spicaofsky@gmail.com>
2025-04-08 16:12:18 -0700

    Update scripts

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 #This script runs the synthesizer on all files in the specified input dir.
 inputs_dir="./tests/synth/test/"
+#All outputs will be placed in the outputs_dir folder
+outputs_dir="./outputs/"
 #Runs in foreground or background
 foreground=0
 
@@ -27,16 +29,16 @@ do
   if [ "$foreground" -eq 1 ]; then
     echo synth-transfer ${entry} -num_programs ${num_programs} -total_rounds ${total_rounds} -num_iters ${num_iters} -condition_length \
      ${condition_length} -solution_size ${solution_size} -num_abd_procs ${num_abd_procs} -weighted_dsl -random_seed ${random_seed}\
-      -bitwidth ${bitwidth} -program_length ${program_length} -outputs_folder ${file_base_name}
+      -bitwidth ${bitwidth} -program_length ${program_length} -outputs_folder ${outputs_dir}${file_base_name}
     synth-transfer ${entry} -num_programs ${num_programs} -total_rounds ${total_rounds} -num_iters ${num_iters} -condition_length \
      ${condition_length} -solution_size ${solution_size} -num_abd_procs ${num_abd_procs} -weighted_dsl -random_seed ${random_seed}\
-      -bitwidth ${bitwidth} -program_length ${program_length} -outputs_folder ${file_base_name}
+      -bitwidth ${bitwidth} -program_length ${program_length} -outputs_folder ${outputs_dir}${file_base_name}
   else
     echo synth-transfer ${entry} -num_programs ${num_programs} -total_rounds ${total_rounds} -num_iters ${num_iters} -condition_length \
      ${condition_length} -solution_size ${solution_size} -num_abd_procs ${num_abd_procs} -weighted_dsl -random_seed ${random_seed}\
-      -bitwidth ${bitwidth} -program_length ${program_length} -outputs_folder ${file_base_name} &
+      -bitwidth ${bitwidth} -program_length ${program_length} -outputs_folder ${outputs_dir}${file_base_name} &
     synth-transfer ${entry} -num_programs ${num_programs} -total_rounds ${total_rounds} -num_iters ${num_iters} -condition_length \
      ${condition_length} -solution_size ${solution_size} -num_abd_procs ${num_abd_procs} -weighted_dsl -random_seed ${random_seed}\
-      -bitwidth ${bitwidth} -program_length ${program_length}  -outputs_folder ${file_base_name} &
+      -bitwidth ${bitwidth} -program_length ${program_length}  -outputs_folder ${outputs_dir}${file_base_name} &
   fi
 done
