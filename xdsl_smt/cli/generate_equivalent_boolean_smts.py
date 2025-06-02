ea7ae06b69c6daa9952573bca59f05f3ee72366c
MDLC01 <57839069+MDLC01@users.noreply.github.com>
2025-06-02 14:51:59 +0100

    New superfluous program removal algorithm

--- xdsl_smt/cli/generate_equivalent_boolean_smts.py
+++ xdsl_smt/cli/generate_equivalent_boolean_smts.py
@@ -10,14 +10,8 @@ from multiprocessing import Pool
 from typing import Generator, Iterable
 
 from xdsl.context import Context
-from xdsl.ir import Operation
 from xdsl.ir.core import BlockArgument, OpResult, SSAValue
 from xdsl.parser import Parser
-from xdsl.pattern_rewriter import (
-    op_type_rewrite_pattern,
-    PatternRewriter,
-    RewritePattern,
-)
 
 from xdsl_smt.dialects.smt_dialect import BoolAttr
 from xdsl_smt.dialects.smt_bitvector_dialect import SMTBitVectorDialect
@@ -26,7 +20,7 @@ from xdsl_smt.dialects.smt_bitvector_dialect import SMTBitVectorDialect
 from xdsl_smt.dialects.smt_utils_dialect import SMTUtilsDialect
 import xdsl_smt.dialects.synth_dialect as synth
 from xdsl.dialects.builtin import Builtin, ModuleOp
-from xdsl.dialects.func import Func, FuncOp, ReturnOp
+from xdsl.dialects.func import Func, FuncOp
 from xdsl_smt.cli.xdsl_smt_run import interpret_module
 
 
@@ -142,10 +136,22 @@ def func_len(func: FuncOp) -> int:
     return len(func_ops(func))
 
 
-def value_matches(lhs_value: SSAValue, prog_value: SSAValue) -> bool:
+def value_matches(
+    argument_values: list[SSAValue | None], lhs_value: SSAValue, prog_value: SSAValue
+) -> bool:
     match lhs_value, prog_value:
-        case BlockArgument(index=i), BlockArgument(index=j):
-            return i == j
+        case BlockArgument(index=i), x:
+            # We "unify" the LHS argument with the corresponding program
+            # value.
+            if len(argument_values) <= i:
+                print(argument_values, lhs_value, i)
+            if argument_values[i] is None:
+                argument_values[i] = x
+                return True
+            # TODO: Use `is_structurally_equivalent` here instead? Does it
+            # handle block arguments properly? E.g., `%arg0` is not the same as
+            # `%arg1`.
+            return argument_values[i] == x
         case OpResult(op=lhs_op, index=i), OpResult(op=prog_op, index=j):
             # TODO: Take attributes into account.
             if i != j:
@@ -157,165 +163,80 @@ def value_matches(lhs_value: SSAValue, prog_value: SSAValue) -> bool:
             for lhs_operand, prog_operand in zip(
                 lhs_op.operands, prog_op.operands, strict=True
             ):
-                if not value_matches(lhs_operand, prog_operand):
+                if not value_matches(argument_values, lhs_operand, prog_operand):
                     return False
             return True
         case _:
             return False
 
 
-class PeepholeRewriter(RewritePattern):
-    lhs_ret: ReturnOp
-    rhs_ops: list[Operation]
-    rhs_res: tuple[SSAValue, ...]
+def program_contains_lhs(program: FuncOp, lhs: FuncOp) -> bool:
+    # TODO: Do we want to make this optimization. As long as our programs aren't
+    #  guaranteed to be DAGs, I'm not sure.
+    # if func_len(lhs) > func_len(program):
+    #     return False
 
-    def __init__(self, lhs: FuncOp, rhs: FuncOp):
-        assert len(lhs.body.blocks) == 1
-        assert len(rhs.body.blocks) == 1
-
-        lhs_ret = lhs.get_return_op()
-        assert lhs_ret is not None
-        self.lhs_ret = lhs_ret
-
-        rhs_ret = rhs.get_return_op()
-        assert rhs_ret is not None
-        # The last operation is a return.
-        self.rhs_ops = list(rhs.body.blocks[0].ops)[:-1]
-        self.rhs_res = rhs_ret.arguments
-
-    @op_type_rewrite_pattern
-    def match_and_rewrite(self, op: Operation, rewriter: PatternRewriter) -> None:
-        # We assume operands have no side-effect.
-        if len(self.lhs_ret.arguments) != len(op.results):
-            return
-        for lhs_op, prog_op in zip(self.lhs_ret.arguments, op.results, strict=True):
-            if not value_matches(lhs_op, prog_op):
-                return
-        # Add all operations from the LHS
-        # TODO: Make sure `self.rhs_res` still has a meaning when it's used in
-        #  another block.
-        rewriter.replace_matched_op([op.clone() for op in self.rhs_ops], self.rhs_res)
-
-
-def try_rewrite(lhs: ModuleOp, rhs: ModuleOp, program: ModuleOp) -> ModuleOp | None:
-    new_program = program.clone()
-    for op in func_ops(get_inner_func(new_program)):
-        rewriter = PatternRewriter(op)
-        PeepholeRewriter(get_inner_func(lhs), get_inner_func(rhs)).match_and_rewrite(
-            op, rewriter
-        )
-        if rewriter.has_done_action:
-            # print(get_inner_func(lhs))
-            # print(get_inner_func(rhs))
-            # print(get_inner_func(program))
-            # print(get_inner_func(new_program))
-            # print()
-            return new_program
-    return None
-
-
-def remove_superfluous(bucket: list[ModuleOp]) -> int:
-    # Programs appear in the bucket by increasing number of operations. The
-    # exact order of programs in the bucket is used when arbitrary decisions
-    # have to be made. This arbitrary total strict order on programs is denoted
-    # by <. It is a superset of the strict order on program induced by their
-    # sizes.
-    bucket.sort(key=lambda m: func_len(get_inner_func(m)))
-
-    # Remove any program that can be rewriten into another program of the bucket
-    # using a single rewrite rule that arises from other programs of the bucket.
-    removed_count = 0
-    removed = [False] * len(bucket)
-    for k, q1m in enumerate(bucket):
-        print(f" {k}/{len(bucket)}, removed {removed_count}", end="\r")
-        q1 = get_inner_func(q1m)
-        for i, p1m in enumerate(bucket):
-            p1 = get_inner_func(p1m)
-            if func_len(p1) >= func_len(q1):
-                break
-            # |p1| < |q1|
-            for p2m in bucket[:i]:
-                # p2 < p1
-                q2m = try_rewrite(p1m, p2m, q1m)
-                if q2m is None:
-                    continue
-                # q1 ~>_p1^p2 q2
-                if len(list(filter(q2m.is_structurally_equivalent, bucket))) != 0:
-                    continue
-                # q2 ∈ initial_bucket
-                q2 = get_inner_func(q2m)
-                # Should be the case because p2 < p1, meaning |p2| <= |p1| and
-                # therefore |q2| <= |q1|.
-                assert func_len(q2) <= func_len(q1)
-                # |q2| <= |q1|
-                if removed[k]:
-                    continue
-                removed[k] = True
-                removed_count += 1
-                # If |q1| = |q2|, is it possible that we later discover that
-                # q2 ~>_p'1^p'2 q1 for some p'2 < p'1, and thus remove q2 as
-                # well?
-                # No. We must have (p'1, p'2) = (p2, p1), and thus p'1 < p'2,
-                # which prevents us from getting to this point.
-
-    # Mutate the argument
-    new_bucket = [module for module, remove in zip(bucket, removed) if not remove]
-    del bucket[:]
-    bucket.extend(new_bucket)
-
-    return removed_count
-
-
-def matches(lhs: ModuleOp, program: ModuleOp) -> bool:
-    # We assume operands have no side-effect.
-
-    lhs_ret = get_inner_func(lhs).get_return_op()
+    lhs_ret = lhs.get_return_op()
     assert lhs_ret is not None
 
-    prog = get_inner_func(program)
+    prog = program
     assert len(prog.body.blocks) == 1
 
     for op in prog.body.blocks[0].ops:
+        argument_values: list[SSAValue | None] = [None] * len(lhs.args)
         if len(lhs_ret.arguments) == len(op.results) and all(
-            map(lambda l, p: value_matches(l, p), lhs_ret.arguments, op.results)
+            map(
+                lambda l, p: value_matches(argument_values, l, p),
+                lhs_ret.arguments,
+                op.results,
+            )
         ):
             return True
     return False
 
 
-def remove_superfluous_2(buckets: Iterable[list[ModuleOp]]) -> int:
-    # We assume all buckets are sorted by program size (this is done by
-    # `remove_superfluous`). The exact order of programs in buckets is used when
-    # arbitrary decisions have to be made. This arbitrary total strict order on
-    # programs of a bucket is denoted by <. It is a superset of the strict order
-    # on programs induced by their sizes. The _canonical representant_ of a
-    # bucket is the first program of that bucket. It is guaranteed to be present
-    # in the superfluous-less bucket.
+def is_program_superfluous(
+    buckets: Iterable[list[ModuleOp]],
+    program: ModuleOp,
+    program_bucket: int,
+    program_index: int,
+) -> bool:
+    for i, bucket in enumerate(buckets):
+        for k, lhs in enumerate(bucket):
+            # Don't try to find a canonical representative in the program.
+            if k == 0:
+                continue
+            # Don't try to match a program with itself, or with any further
+            # program in its bucket.
+            if i == program_bucket and k >= program_index:
+                break
+            if program_contains_lhs(get_inner_func(program), get_inner_func(lhs)):
+                return True
+    return False
+
+
+def remove_superfluous(buckets: Iterable[list[ModuleOp]]) -> int:
+    # This defines a total order on the programs of a bucket. The first program
+    # of a bucket is called its _canonical representative_, and it is the only
+    # program of that bucket that is allowed to appear as a strict subprogram of
+    # any program.
+    for bucket in buckets:
+        bucket.sort(key=lambda m: func_len(get_inner_func(m)))
 
     removed_count = 0
+    for i, bucket in enumerate(buckets):
+        # We can modify `bucket` inside this loop because we loop over a copy.
+        # Since we iterate backwards, we can remove the elements by index.
+        for k, program in reversed(list(enumerate(bucket))[1:]):
+            if is_program_superfluous(buckets, program, i, k):
+                removed_count += 1
+                del bucket[k]
+            print(
+                f"\033[2K Bucket {i}, program {k} (removed {removed_count} in total)",
+                end="\r",
+            )
 
-    for i, program_bucket in enumerate(buckets):
-        remove_indices: list[int] = []
-        for k, program in enumerate(program_bucket):
-            # If we can match the program with any non-canonical representant of
-            # a different bucket, we remove the program.
-            # This is correct because the matched sub-program can be rewriten to
-            # the canonical representant of the LHS's bucket to create a new
-            # program P' whose size does not exceed that of `program`.
-            # Therefore, P' was enumerated. Now, suppose we remove P' from this
-            # bucket. This has to be because it matched an LHS. This LHS cannot
-            # be the canonical representant of a bucket, so there exists a
-            # program P'' in this bucket with P'' != P' and P'' != `program`
-            # that can be obtained from `program` by applying rewrite rules.
-            for j, lhs_bucket in enumerate(buckets):
-                if i == j:
-                    continue
-                if any(map(lambda lhs: matches(lhs, program), lhs_bucket)):
-                    remove_indices.append(k)
-                    removed_count += 1
-                    break
-        for l in reversed(remove_indices):
-            del program_bucket[l]
+    print("\033[2K", end="\r")
 
     return removed_count
 
@@ -370,16 +291,11 @@ def main() -> None:
 
         # Write disk files for each bucket.
         print("Removing superfluous programs...")
-        for images, bucket in buckets.items():
-            initial_bucket_size = len(bucket)
-            removed_count = remove_superfluous(bucket)
-            print(
-                f"Removed {removed_count}/{initial_bucket_size} programs from bucket {images}"
-            )
-        additional_removal_count = remove_superfluous_2(buckets.values())
-        print(
-            f"Removed {additional_removal_count} additional programs from all buckets"
-        )
+        removed_count = remove_superfluous(buckets.values())
+        print(f"Removed {removed_count} programs from all buckets")
+
+        remaining_count = sum(len(bucket) for bucket in buckets.values())
+        print(f"Remaining programs: {remaining_count}")
 
         for images, bucket in buckets.items():
             if not os.path.exists(args.out_dir):
@@ -395,7 +311,7 @@ def main() -> None:
                 f.write("\n")
                 for module in bucket:
                     f.write(str(module))
-                    f.write("// -----\n")
+                    f.write("\n// -----\n")
 
     except BrokenPipeError as e:
         # The enumerator has terminated
